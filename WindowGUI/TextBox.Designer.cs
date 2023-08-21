065a109d053c5b8afdeb3f3656193dfaef3044a9
VietHoang <trinhviethoang16@gmail.com>
2023-08-21 19:35:34 +0700

    add textbox

---
+++ WindowGUI/TextBox.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowGUI
+{
+    partial class TextBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(75, 65);
+            this.txtBox.Multiline = true;
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(100, 20);
+            this.txtBox.TabIndex = 0;
+            this.txtBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(181, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbValue
+            // 
+            this.lbValue.AutoSize = true;
+            this.lbValue.Location = new System.Drawing.Point(110, 100);
+            this.lbValue.Name = "lbValue";
+            this.lbValue.Size = new System.Drawing.Size(31, 13);
+            this.lbValue.TabIndex = 2;
+            this.lbValue.Text = "none";
+            // 
+            // TextBox
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 294);
+            this.Controls.Add(this.lbValue);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtBox);
+            this.Name = "TextBox";
+            this.Text = "TextBox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbValue;
+    }
+}
---
+++ WindowGUI/TextBox.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowGUI
+{
+    public partial class TextBox : Form
+    {
+        public TextBox()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int num = 0;
+
+            if(Int32.TryParse(txtBox.Text, out num))
+            {
+                lbValue.Text = (num * num).ToString();
+            }
+            else
+            {
+                lbValue.Text = "Wrong input";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbValue.Text = txtBox.Text;
+        }
+    }
+}
