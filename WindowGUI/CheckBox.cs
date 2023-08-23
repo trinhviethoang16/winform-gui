3b2611cd6a9a533b5e8cb876c729a5a2ee610a4b
VietHoang <trinhviethoang16@gmail.com>
2023-08-23 16:25:53 +0700

    add checkbox

---
+++ WindowGUI/CheckBox.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowGUI
+{
+    public partial class CheckBox : Form
+    {
+        public CheckBox()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string isGay = checkBox1.CheckState == CheckState.Checked ? "is" :
+                checkBox1.CheckState == CheckState.Unchecked ? "is not" : "is and not";
+            string showString = string.Format("Hello {0}, you {1} gay !", name, isGay);
+            MessageBox.Show(showString);
+        }
+    }
+}
