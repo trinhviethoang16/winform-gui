db22f01cc585e838bfa852700f5b7d4d5067bd79
VietHoang <trinhviethoang16@gmail.com>
2023-08-23 20:00:38 +0700

    add picturebox

---
+++ WindowGUI/PictureBoxGUI.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowGUI
+{
+    partial class PictureBoxGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBoxImage = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(152, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 209);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBoxImage
+            // 
+            this.comboBoxImage.FormattingEnabled = true;
+            this.comboBoxImage.Location = new System.Drawing.Point(12, 102);
+            this.comboBoxImage.Name = "comboBoxImage";
+            this.comboBoxImage.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxImage.TabIndex = 1;
+            this.comboBoxImage.SelectedValueChanged += new System.EventHandler(this.comboBoxImage_SelectedValueChanged);
+            // 
+            // PictureBoxGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 309);
+            this.Controls.Add(this.comboBoxImage);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "PictureBoxGUI";
+            this.Text = "PictureBoxGUI";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBoxImage;
+    }
+}
---
+++ WindowGUI/PictureBoxGUI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowGUI
+{
+    public partial class PictureBoxGUI : Form
+    {
+        private string extention = ".jpg";
+        public PictureBoxGUI()
+        {
+            InitializeComponent();
+            loadImageName();
+        }
+
+        private void loadImageName()
+        {
+            List<string> listImages = new List<string>() { "1", "2", "3", "4", "5" };
+            comboBoxImage.DataSource = listImages;
+        }
+
+        private void comboBoxImage_SelectedValueChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = sender as ComboBox;
+            if (cb.SelectedValue != null)
+            {
+                Bitmap bitmap = new Bitmap(Application.StartupPath + "\\Resources\\" 
+                    + cb.SelectedValue.ToString() + extention);
+                pictureBox1.Image = bitmap;
+            }
+        }
+    }
+}
