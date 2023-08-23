07994259e39f531a69229731b6e3142f7b44e7f2
VietHoang <trinhviethoang16@gmail.com>
2023-08-23 19:36:58 +0700

    add combobox

---
+++ WindowGUI/ComboBoxGUI.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowGUI
+{
+    partial class ComboBoxGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboClass = new System.Windows.Forms.ComboBox();
+            this.comboStudent = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(70, 95);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(70, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add list items";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(218, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // comboClass
+            // 
+            this.comboClass.FormattingEnabled = true;
+            this.comboClass.Location = new System.Drawing.Point(70, 172);
+            this.comboClass.Name = "comboClass";
+            this.comboClass.Size = new System.Drawing.Size(121, 21);
+            this.comboClass.TabIndex = 3;
+            this.comboClass.SelectedValueChanged += new System.EventHandler(this.comboClass_SelectedValueChanged);
+            // 
+            // comboStudent
+            // 
+            this.comboStudent.FormattingEnabled = true;
+            this.comboStudent.Location = new System.Drawing.Point(218, 172);
+            this.comboStudent.Name = "comboStudent";
+            this.comboStudent.Size = new System.Drawing.Size(121, 21);
+            this.comboStudent.TabIndex = 4;
+            // 
+            // ComboBoxGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 364);
+            this.Controls.Add(this.comboStudent);
+            this.Controls.Add(this.comboClass);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "ComboBoxGUI";
+            this.Text = "ComboBoxGUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboClass;
+        private System.Windows.Forms.ComboBox comboStudent;
+    }
+}
---
+++ WindowGUI/ComboBoxGUI.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowGUI
+{
+    public partial class ComboBoxGUI : Form
+    {
+
+        private List<Food> list;
+        private List<ComboBoxClass> listClass;
+        public ComboBoxGUI()
+        {
+            InitializeComponent();
+
+            //addBinding();
+            //list = new List<Food>()
+            //{
+            //    new Food() { Name = "Thit heo", Price = 1000 },
+            //    new Food() { Name = "Thit bo", Price = 2000 },
+            //    new Food() { Name = "Thit ga", Price = 500 }
+            //};
+            //comboBox1.DataSource = list;
+            //comboBox1.DisplayMember = "Name";
+
+            listClass = new List<ComboBoxClass>();
+            listClass.Add(new ComboBoxClass()
+            {
+                ClassName = "A",
+                ListStudents = new List<string>() { "Hoang", "Huy", "Nguyen", "Son" }
+            });
+            listClass.Add(new ComboBoxClass()
+            {
+                ClassName = "B",
+                ListStudents = new List<string>() { "Khoa", "Khang", "Duy", "Quan" }
+            });
+            listClass.Add(new ComboBoxClass()
+            {
+                ClassName = "C",
+                ListStudents = new List<string>() { "Tri", "Doan", "Man", "Hien" }
+            });
+            comboClass.DataSource = listClass;
+            comboClass.DisplayMember = "ClassName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            list = new List<Food>()
+            {
+                new Food() { Name = "Thit heo", Price = 1000 },
+                new Food() { Name = "Thit bo", Price = 2000 },
+                new Food() { Name = "Thit ga", Price = 500 }
+            };
+            comboBox1.DataSource = list;
+            comboBox1.DisplayMember = "Name";
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = sender as ComboBox;
+            if (cb.SelectedValue != null)
+            {
+                Food food = cb.SelectedValue as Food;
+                textBox1.Text = food.Price.ToString();
+            }
+        }
+
+        private void comboClass_SelectedValueChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = sender as ComboBox;
+            if (cb.SelectedValue != null)
+            {
+                ComboBoxClass cbl = cb.SelectedValue as ComboBoxClass;
+                comboStudent.DataSource = cbl.ListStudents;
+            }
+        }
+
+        //private void addBinding()
+        //{
+        //    textBox1.DataBindings.Add(new Binding("Text", comboBox1.DataSource, "Price"));
+        //}
+    }
+
+    public class Food
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+    }
+
+    public class ComboBoxClass
+    {
+        public string ClassName { get; set; }
+        public List<string> ListStudents { get; set; }
+    }
+}
