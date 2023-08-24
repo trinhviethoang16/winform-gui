ba051bc66cb7b28d201506386b6d74a062799dce
VietHoang <trinhviethoang16@gmail.com>
2023-08-24 17:52:37 +0700

    add numbericupdown

---
+++ WindowGUI/NumericUpDownGUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowGUI
+{
+    public partial class NumericUpDownGUI : Form
+    {
+        public NumericUpDownGUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
