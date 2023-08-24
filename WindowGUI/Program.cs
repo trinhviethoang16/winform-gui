e5a678a9e15f0c80a6edc57415d7b1ec5986e4fd
VietHoang <trinhviethoang16@gmail.com>
2023-08-24 17:12:17 +0700

    add timer

--- WindowGUI/Program.cs
+++ WindowGUI/Program.cs
@@ -16,7 +16,7 @@ namespace WindowGUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ThreadGUI());
+            Application.Run(new TimerGUI());
         }
     }
 }
---
+++ WindowGUI/TimerGUI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowGUI
+{
+    public partial class TimerGUI : Form
+    {
+        Timer timer;
+        private int i = 0;
+        public TimerGUI()
+        {
+            InitializeComponent();
+            timer = new Timer();
+            timer.Tick += timer1_Tick;
+            timer.Interval = 1000;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = !timer.Enabled;
+            button1.Text = button1.Text == "Stop" ? "Start" : "Stop";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            i++;
+            label1.Text = i.ToString();
+        }
+    }
+}
